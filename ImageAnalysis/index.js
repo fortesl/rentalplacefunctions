909f689aade123f7df8fe04cbf8a1843d1a8fd9f
Fortes, Luis - Dell Team <L_Fortes@DellTeam.com>
2019-07-18 17:26:40 -0500

    Initial commit

---
+++ ImageAnalysis/index.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+module.exports = async function (context, myBlob) {
+    context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
+
+    const options = {
+        url: "https://rentalplaceface.cognitiveservices.azure.com/face/v1.0/detect?returnFaceAttributes=emotion&returnFaceId=false",
+        method: "POST",
+        body: myBlob,
+        headers: {
+            "Content-Type": "appication/octet-stream",
+            "Ocp-Apim-Subscription-Key": process.env.COGNITIVE_SERVICES || "7a3da4d72c1e4d77a336551b51b56e0b"
+        }
+    };
+
+    request(options, (err, results) => {
+        if (err) return context.done();
+
+        context.log(results.body);
+
+        context.done();
+
+    });
+};
